f0c4e464a22f878f601abd52f9be0ad0e85f2e24
worldze <61602144+worldze@users.noreply.github.com>
2020-03-22 15:48:21 -0600

    Updates

---
+++ precache-manifest.0f23befbd07175e4bbefb3ff0bbd51a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61aaeeb2ffc94f057ce16c1e6791b0a",
+    "url": "/you/index.html"
+  },
+  {
+    "revision": "1cc880f59f2df343dc8e",
+    "url": "/you/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb5c9212dd03ecb6920d",
+    "url": "/you/static/js/2.86f570fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/you/static/js/2.86f570fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc880f59f2df343dc8e",
+    "url": "/you/static/js/main.f0fb918b.chunk.js"
+  },
+  {
+    "revision": "93c1c86f960411b8fbdc",
+    "url": "/you/static/js/runtime-main.bf6f3757.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/you/static/media/logo.5d5d9eef.svg"
+  }
+]);
